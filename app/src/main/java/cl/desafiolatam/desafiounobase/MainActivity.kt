4846949dc8843085fe1f5456cd3c0b9fa72f350a
foxhunter <kevin.floba@gmail.com>
2020-08-17 16:50:23 -0400

    Volver al Main Activity si tu nombre está

---
+++ app/src/main/java/cl/desafiolatam/desafiounobase/MainActivity.kt
@@ -0,0 +1,62 @@
+package cl.desafiolatam.desafiounobase
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var nameInput: TextInputEditText
+    lateinit var advance: Button
+    lateinit var container: ConstraintLayout
+    lateinit var preferences: SharedPreferences
+    val filename = "cl.desafiolatam.desafiounobase"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        preferences = getSharedPreferences(filename, Context.MODE_PRIVATE)
+        nameInput = findViewById(R.id.name_input)
+        advance = findViewById(R.id.login_button)
+        container = findViewById(R.id.container)
+        setUpListeners()
+    }
+
+    private fun setUpListeners() {
+        advance.setOnClickListener {
+            if (nameInput.text!!.isNotEmpty()) {
+                val intent: Intent
+                if (hasSeenWelcome()) {
+                    intent = Intent(this, HomeActivity::class.java)
+                } else {
+                    saveUser(name_input.text.toString())
+                    intent = Intent(this, WelcomeActivity::class.java)
+                }
+                startActivity(intent)
+            } else {
+                Snackbar.make(container, "El nombre no puede estar vacío", Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun hasSeenWelcome(): Boolean {
+        val stringSet = preferences.getStringSet("Usuarios", setOf())
+        //implementar este método para saber si el usuario ya ha entrado a la aplicación y ha visto
+        //la pantalla de bienvenida. Este método permite decidir que pantalla se muestra después de presionar Ingresar
+        //recorra la lista de usuarios
+        return stringSet.contains(name_input.text.toString())
+    }
+
+    private fun saveUser(userName: String) {
+        val setString = preferences.getStringSet("Usuarios", mutableSetOf())
+        setString.add(userName)
+        preferences.edit().putStringSet("Usuarios", setString).apply()
+    }
+}
